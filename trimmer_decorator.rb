82deabc85a9080113f0a12fbaf3805c73230ce7f
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-07-24 16:30:05 -0400

    Fix required changes

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,7 +2,7 @@ require_relative 'base_decorator'
 
 class TrimmerDecorator < BaseDecorator
   def correct_name
-    return @nameable.correct_name.strip if @nameable.correct_name.length > 10
+    return @nameable.correct_name[0, 10] if @nameable.correct_name.length > 10
 
     @nameable.correct_name
   end
